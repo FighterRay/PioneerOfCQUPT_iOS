12e5aba2b7e7f941616bc36c9d19f4d93d9bc2ea
Johnlee666 <895471139@qq.com>
2016-06-20 23:50:33 +0800

    学习资料

---
+++ PioneerOfCQUPT_iOS/Feature/Community/DetailViewController.h
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.h
+//  PioneerOfCQUPT_iOS
+//
+//  Created by 李展 on 16/6/20.
+//  Copyright © 2016年 张润峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+@property NSString *detailTitle;
+@property NSString *detailContent;
+@end
---
+++ PioneerOfCQUPT_iOS/Feature/Community/LearningMaterialsCell.m
@@ -0,0 +1,24 @@
+//
+//  LearningMaterialsCell.m
+//  PioneerOfCQUPT_iOS
+//
+//  Created by 李展 on 16/6/20.
+//  Copyright © 2016年 张润峰. All rights reserved.
+//
+
+#import "LearningMaterialsCell.h"
+
+@implementation LearningMaterialsCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
